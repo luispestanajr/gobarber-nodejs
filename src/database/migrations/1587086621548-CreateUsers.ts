8b5cf7899d135c3652b183a6543df32426054519
Luis Antonio <luis.pestanajr@gmail.com>
2020-04-17 00:28:28 -0300

    Cadastro de Usuários

--- src/database/migrations/1587086621548-CreateUsers.ts
+++ src/database/migrations/1587086621548-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1586908240495
-  implements MigrationInterface {
+export default class CreateUsers1587086621548 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,19 @@ export default class CreateAppointments1586908240495
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
